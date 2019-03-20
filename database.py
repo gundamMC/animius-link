fbd57fe95004801321c8585159f72ea1f755055a
LXG-Shadow <32156054+LXG-Shadow@users.noreply.github.com>
2019-03-20 13:34:35 +0800

    Add user table fix notes table

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, String, Integer, Boolean, create_engine, ForeignKey, Text
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker, relationship,session as s
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import ProgrammingError
-import time
+import time,sqlalchemy
 
 Base = declarative_base()
 conn = "sqlite:///Animius_Link.db"
@@ -10,7 +10,7 @@ conn = "sqlite:///Animius_Link.db"
 engine = create_engine(conn)
 # create sessionmaker:
 DBSession = sessionmaker(bind=engine)
-session = None
+session = None # type:s.Session
 
 def checkDatabase():
     try:
@@ -42,6 +42,28 @@ def createTables():
         return False
 
 
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(Text,nullable=False,unique=True,index=True)
+    password = Column(Text,nullable=False,index=True)
+    notes = relationship("Notes", backref='user', lazy='dynamic')
+    notes_category = relationship("NotesCategory", backref='user', lazy='dynamic')
+
+    @classmethod
+    def getByName(cls,name):
+        return session.query(cls).filter_by(name=name).first()
+
+    @classmethod
+    def getById(cls,id):
+        return session.query(cls).filter_by(id=id).first()
+
+    def checkPassword(self,psd):
+        return self.password == psd
+
+
+
 class Notes(Base):
     __tablename__ = 'notes'
 
@@ -50,22 +72,37 @@ class Notes(Base):
     note = Column(Text)
     time = Column(Integer)
     category_id = Column(Integer,ForeignKey("note_category.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
 
     @classmethod
-    def getLatest(cls,num=1):
-        return session.query(cls).order_by(Notes.title.desc()).limit(num).all()
+    def getLatest(cls,user,num=1):
+        """
+        :param user: User object
+        :param num:
+        :return:
+        """
+        return session.query(cls).filter_by(user_id = user.id).order_by(Notes.title.desc()).limit(num).all()
 
     @classmethod
-    def getAll(cls):
-        return session.query(Notes).all()
+    def getAll(cls,user):
+        """
+        :param user: User object
+        :return:
+        """
+        return session.query(Notes).filter_by(user_id=user.id).all()
 
     @classmethod
-    def seachByTitle(cls,title):
-        return session.query(Notes).filter_by(title=title).all()
+    def seachByTitle(cls,user,title):
+        """
+        :param user: User object
+        :param title:
+        :return:
+        """
+        return session.query(Notes).filter_by(user_id = user.id,title=title).all()
 
     @classmethod
-    def add(cls, title,note,category_id):
-        note0 = cls(title=title,note=note,time = int(time.time()),category_id=category_id)
+    def add(cls,user,title,note,category):
+        note0 = cls(title=title,note=note,time = int(time.time()),category_id=category.id,user_id=user.id)
         session.add(note0)
         session.commit()
         return True
@@ -76,27 +113,32 @@ class NotesCategory(Base):
     __tablename__ = 'note_category'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
-    name = Column(Text, index=True,unique=True)
+    name = Column(Text, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
     notes = relationship("Notes", backref='category', lazy='dynamic')
 
-    @classmethod
-    def getByName(cls,name):
-        return session.query(NotesCategory).filter_by(name=name).first()
 
     @classmethod
-    def getNotesByName(cls,name):
-        cate = cls.getByName(name) # type: NotesCategory
+    def getByName(cls,user,name):
+        return session.query(NotesCategory).filter_by(user_id=user.id,name=name).first()
+
+    @classmethod
+    def getNotesByName(cls,user,name):
+        cate = cls.getByName(user,name) # type: NotesCategory
         if cate == None:
             return []
         return cate.notes
 
     @classmethod
-    def getNotesById(cls,id):
-        return session.query(NotesCategory).filter_by(id=id).first()
+    def getNotesById(cls,user,id):
+        return session.query(NotesCategory).filter_by(user_id=user.id,id=id).first()
 
     @classmethod
-    def add(cls,name):
-        cate = cls(name=name)
+    def add(cls,user,name):
+        cate = cls.getByName(user,name)
+        if cate != None:
+            return False
+        cate = cls(user_id=user.id,name=name)
         session.add(cate)
         session.commit()
         return True
--- demo2.py
+++ demo2.py
@@ -3,17 +3,38 @@ import database
 database.session = database.DBSession()
 
 #database.createTables()
+user = database.Users.getByName("admin")
+print(user.id,user.name)
+#cate = database.NotesCategory.add(user,"default")
+#cate = database.NotesCategory.add(user,"life")
+#print(cate)
 
-print(database.Notes.getAll()[0].title)
-#database.NotesCategory.add("ceshi")
-database.Notes.add("newnote","sdkhfkasdf",1)
-notesindefault = database.NotesCategory.getNotesByName("default")
-for note in notesindefault:
-    print("-"*3)
+# database.Notes.add(user,"ceshi","23131312",database.NotesCategory.getByName(user,"default"))
+# database.Notes.add(user,"ceshi1","23131312",database.NotesCategory.getByName(user,"default"))
+# database.Notes.add(user,"ceshi2","23131312",database.NotesCategory.getByName(user,"default"))
+# database.Notes.add(user,"ceshil","23131312ll",database.NotesCategory.getByName(user,"life"))
+# database.Notes.add(user,"ceshi1l","23131312lll",database.NotesCategory.getByName(user,"life"))
+# database.Notes.add(user,"ceshi2l","23131312llll",database.NotesCategory.getByName(user,"life"))
+print("totalnotes",user.notes.count())
+for note in user.notes:
+    print("-" * 3)
     print("title",note.title)
     print("time",note.time)
     print("cate",note.category.name)
     print(note.note)
     print()
 
+print("total categoty",user.notes_category.count())
+for cate in user.notes_category:
+    print("*"*3)
+    print("category name",cate.name)
+    print("total notes",cate.notes.count())
+    for note in cate.notes:
+        print("-" * 3)
+        print("title", note.title)
+        print("time", note.time)
+        print("cate", note.category.name)
+        print(note.note)
+    print("8"*3)
 database.session.close()
+
