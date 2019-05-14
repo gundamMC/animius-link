044465d730a779ef232e5ee9cdae5f1d2e414e81
xAsiimov <admin@xasiimov.com>
2019-05-14 19:16:21 +0800

    Update Main.py and _init_.py.
    Signed-off-by: xAsiimov <admin@xasiimov.com>

--- amlink/Main.py
+++ amlink/Main.py
@@ -1,47 +1,7 @@
-import json
-
-from amlink import SocketClient, SocketServer, module_controller
-
-id_username = {}
-ip = '127.0.0.1'
-port = 2333
+import amlink
 
 if __name__ == "__main__":
-    serverThread, serverSendQueue = SocketServer.start_server(port + 1, True)
-    clientThread, clientSendQueue = SocketClient.start_client(ip, port, 'p@ssword')
+    amlink.network_controller.Network()
 
-class NetworkHandler:
-
-    @staticmethod
-    def toEngine(id, cid, username, command, arguments):
-
-        if command == 'waifuPredict':
-            id_username[id] = [cid, username, True]
-        else:
-            id_username[id] = [cid, username, False]
-
-        clientSendQueue.put({'id': id, 'command': command, 'arguments': arguments})
-        # clientThread.client.send(id, command, arguments)
-
-    @staticmethod
-    def toClient(resp):
-        id = resp.id
-        status = resp.status
-        message = resp.message
-        data = resp.data
-        cid = id_username[id][0]
-        username = id_username[id][1]
-        linkProcess = id_username[id][2]
-
-        if message == 'success':
-            if linkProcess and 'intent' in data.keys and 'ner' in data.keys:
-                intent = data['intent']
-                ner = SocketClient.parse_ner(data['ner'][0], data['ner'][1])
-                return_value = module_controller.intents[intent].__call__(ner, username)
-                return_value['intent'] = intent
-
-            else:
-                return_value = resp.data
-
-            return_value = json.dumps(return_value).encode("utf-8")
-            serverThread.clients[cid].send(id, status, message, return_value)
+    while True:
+        pass
--- amlink/__init__.py
+++ amlink/__init__.py
@@ -1,9 +1,8 @@
-from amlink.config import config
-
 import amlink.SocketClient as SocketClient
 import amlink.SocketServer as SocketServer
 import amlink.database_controller as database_controller
 import amlink.module_controller as module_controller
+import amlink.network_controller as network_controller
 import amlink.users as users
 
-from amlink.Main import NetworkHandler
+from amlink.config import config
