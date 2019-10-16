53bcfe4c60823668ec3c5002679ffc99caac30d8
gundamMC <gundammc@qq.com>
2019-10-15 17:15:51 -0700

    Add user creation entry point in main

--- amlink/main.py
+++ amlink/main.py
@@ -1,7 +1,25 @@
 import amlink
+import sys
 
 if __name__ == "__main__":
-    amlink.network_controller.Network()
 
-    while True:
-        pass
+    if len(sys.argv) <= 1:
+        amlink.network_controller.Network()
+
+        while True:
+            pass
+
+    elif sys.argv[1] == 'createuser':
+        username = input('username: ')
+        password = input('password: ')
+
+        print('The following user will be created:')
+        print('Username:', username)
+        print('Password:', password)
+        confirm = input('Do you want to continue? (y/n): ')
+        if confirm == 'y':
+            amlink.users.create_user(username, password)
+            print('user created')
+        else:
+            print('user creation aborted')
+            sys.exit()
